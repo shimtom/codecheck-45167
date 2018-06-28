883e965a4164ed0e535a185f289a53e5ab021037
shimtom <shimtom1230@gmail.com>
2018-06-28 16:05:28 +0900

    add answer

--- app/main.py
+++ app/main.py
@@ -5,5 +5,5 @@ def main(argv):
     # This is a sample code to use arguments and outputs.
     # Edit and remove this code as you like.
 
-    for i, v in enumerate(argv):
-        print("argv[{0}]: {1}".format(i, v))
+    for v in argv:
+        print("Hello {1}!".format(v))
